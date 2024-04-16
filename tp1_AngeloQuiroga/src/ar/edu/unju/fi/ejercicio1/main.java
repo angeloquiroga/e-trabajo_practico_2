57938a590b06708153c0db845d59058c918e3bbe
angel <angel@DESKTOP-V729OP9>
2024-04-15 23:29:44 -0300

    ejercicios de tp1

---
+++ tp1_AngeloQuiroga/src/ar/edu/unju/fi/ejercicio1/main.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+	int a = 2, b = 6 , c = 3; 
+	int d = 9 , e = 15;
+	int promedio;
+	promedio = a + b + c + d + e / 5;
+	System.out.println("El promedio es: " + promedio);
+	}
+
+}
